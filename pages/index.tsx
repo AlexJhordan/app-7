7c11ee177cbad602881b34e8b840fb17167ffb30
Alex <alex.ajct01@gmail.com>
2024-11-13 16:40:14 -0300

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,10 @@ import React from "react";
 function Home() {
   return (
     <div>
-
       <div>Home 2</div>
       <Link href="/sobre">Ir para Sobre</Link>
+      <br/>
+      <Link href="/tempo">Ir para Tempo</Link>
     </div>
   );
 }
